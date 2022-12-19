0fb54f91f4dfc808743cdf6b7fc98ced3cfdd620
Yosief G. Hailay <yosiefghaylay@gmail.com>
2022-12-19 16:56:14 +0300

    first draft mobile menu

--- mobile-menu.js
+++ mobile-menu.js
@@ -1,8 +1,18 @@
-const humburger = document.querySelector(".humberger");
-const navMenu = document.querySelector(".navbar-links");
+let BarMenu = document.querySelector(".humberger");
+let NavMenu = document.querySelector('.nav-links');
 
-humburger.addEventListener("click",() =>{
-    humburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+let NewClass = document.createElement('ul');
+    NewClass.class = "pop-links";
+    
+    let li =document.createElement('li')
+    li.textContent = 'portfolio';
+    let li1 =document.createElement('li')
+    li.textContent = 'about';
+    let li2 =document.createElement('li')
+    li.textContent = 'contact';
 
-})
+    NewClass.appendChild(li);
+    NewClass.appendChild(li1);
+    NewClass.appendChild(li2);
+    
+BarMenu.appendChild(NewClass);
